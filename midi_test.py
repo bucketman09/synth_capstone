4ee446e32078cead27650d941c0c30a76ac22317
bucketman09 <njsprouse20@gmail.com>
2025-04-19 13:29:31 -0400

    user input

--- midi_test.py
+++ midi_test.py
@@ -5,8 +5,10 @@ midi_in = rtmidi.MidiIn()
 
 print(midi_in.get_ports())
 
+midi_device = input("Select device (Starting index 0)")
+
 try:
-    midi_in.open_port(0)
+    midi_in.open_port(midi_device)
 except Exception as e:
     print("no midi instruments detected")
 
