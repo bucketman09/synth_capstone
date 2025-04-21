b3fb8cd5b2a592b3ce9f0afd80b6d5bfc8a1dc03
bucketman09 <njsprouse20@gmail.com>
2025-04-20 22:05:29 -0400

    testing oledScreen

---
+++ GUI.py
@@ -0,0 +1,35 @@
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+import numpy as np
+
+class GUI
+    def __init__ (self, CHUNK, AMP):
+        self.chunk = CHUNK
+        self.s_w = 128
+        self.s_h = 64
+        self.x_r = (CHUNK/s_w)
+        self.y_r = ((s_h/2) / AMP)
+        
+        self.serial = i2c(port=1, address=0x3C)
+        self.device = ssd1306(serial, rotate=0)
+        
+    def draw_wave(self, wave):
+        #x_r = 256/128
+        #y_r = 32/amp
+        screen = np.zeros(chunk)
+        
+        for i in range(len(screen)):
+            screen[i] = (wave[i] + wave[int(i + self.x_r)])/2
+            screen[i] = (screen[i] * self.y_r) + 32
+           
+        with canvas(self.device) as draw:
+                draw.point(graph, fill="white")
+            
+        
+      
+
+        
+        
+        
+
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import rtmidi
-import pyaudio
+#import pyaudio
 import numpy as np
 
 from note import Note
 from adsr_envelope import Env
 from osc import Osc
+from GUI import GUI
+from osc import Osc
+
+
 
 adsr = Env(.5,.5,.5,1)
 midi_in = rtmidi.MidiIn()
@@ -16,12 +20,13 @@ CHUNK = 256
 amp = 32767
 freq = 440
 
+gui = GUI(CHUNK,amp)
 osc = Osc()
 num_aud_osc = 1
 
-p = pyaudio.PyAudio()
+#p = pyaudio.PyAudio()
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate = RATE, input=False, output=True, frames_per_buffer=CHUNK)
+#stream = p.open(format=pyaudio.paInt16, channels=1, rate = RATE, input=False, output=True, frames_per_buffer=CHUNK)
 
 pressed = False
 
@@ -79,6 +84,7 @@ if midi_in.is_port_open():
                 
                 wave += osc.generate_wf(0, amp, freq, t_values)
                 
+        gui.draw_wave(wave)
         
         #normalize amp
         if len(notes) != 0: 
@@ -86,7 +92,7 @@ if midi_in.is_port_open():
             
         wave = wave.astype(np.int16)
         
-        stream.write(wave.tobytes())
+        #stream.write(wave.tobytes())
         
         #prevent excesive t size
         #if np.all(wave == 0):
@@ -94,7 +100,7 @@ if midi_in.is_port_open():
             
         t += CHUNK
                    
-stream.stop_stream()
-stream.close()
-p.terminate()
+#stream.stop_stream()
+#stream.close()
+#p.terminate()
         
