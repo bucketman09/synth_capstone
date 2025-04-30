444632f782f91efca1b46a29fb95c895c1bb4b3a
bucketman09 <njsprouse20@gmail.com>
2025-04-29 22:59:38 -0400

    changed test again

--- input_test.py
+++ input_test.py
@@ -1,19 +1,21 @@
 from gpiozero import Button, RotaryEncoder
-import signal as s
+from signal import pause
 
-
-rotor = RotaryEncoder(10,9)
+rotor = RotaryEncoder(10,9,wrap=True)
 rotor_btn = Button(11)
 
 def pressed():
     print("pressed")
     
-def rotated():
-    print(rotor.values)
+def left():
+    print("left/down")
     
-rotor.when_rotated = rotated
+def right():
+    print("right/up")
     
+rotor.when_rotated_clockwise = right
+rotor.when_rotated_counter_clockwise = left
 rotor_btn.when_pressed = pressed
 
-s.pause()
+pause()
     
