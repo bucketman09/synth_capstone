e46e10e1b2a2b1d9a2c5865a42b691e109f2ebd9
bucketman09 <njsprouse20@gmail.com>
2025-04-29 22:44:45 -0400

    change input test

--- input_test.py
+++ input_test.py
@@ -7,6 +7,11 @@ rotor_btn = Button(11)
 def pressed():
     print("pressed")
     
+def rotated():
+    print(rotor.values)
+    
+rotor.when_rotated = rotated
+    
 rotor_btn.when_pressed = pressed
 
 while True:
